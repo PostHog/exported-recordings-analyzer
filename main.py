f748ce3c4665cfc718a50b69de459a7f326a5467
Paul D'Ambra <paul@posthog.com>
2023-10-23 18:54:18 +0100

    allow analysing s3 downloaded files

--- main.py
+++ main.py
@@ -69,8 +69,8 @@ class SizedCount:
     def combine(self, other: "SizedCount") -> "SizedCount":
         return SizedCount(self.count + other.count, self.size + other.size)
 
-    def __add__(self, bytes: int) -> "SizedCount":
-        return SizedCount(self.count + 1, self.size + bytes)
+    def __add__(self, bytes_to_add: int) -> "SizedCount":
+        return SizedCount(self.count + 1, self.size + bytes_to_add)
 
     def __str__(self) -> str:
         return f"{self.count} ({sizeof_fmt(self.size)})"
@@ -98,24 +98,33 @@ class UnterminatedLine:
 
 @dataclasses.dataclass()
 class Analysis:
-    message_type_counts: Dict[str, int] = dataclasses.field(default_factory=dict)
-    incremental_snapshot_event_source_counts: Dict[str, SizedCount] = dataclasses.field(
-        default_factory=dict
-    )
-    mutation_addition_counts: Dict[str, SizedCount] = dataclasses.field(
-        default_factory=dict
-    )
-    mutation_attributes_counts: Dict[str, SizedCount] = dataclasses.field(
-        default_factory=dict
-    )
-    addition_sizes: List[int] = dataclasses.field(default_factory=list)
-    mutation_removal_count: int = 0
-    unterminated_lines: List[UnterminatedLine] = dataclasses.field(default_factory=list)
+    message_type_counts: Dict[str, int]
+    incremental_snapshot_event_source_counts: Dict[str, SizedCount]
+    mutation_addition_counts: Dict[str, SizedCount]
+    mutation_attributes_counts: Dict[str, SizedCount]
+    addition_sizes: List[int]
+    mutation_removal_count: SizedCount
+    text_mutation_count: SizedCount
+    unterminated_lines: List[UnterminatedLine]
+
+    @staticmethod
+    def empty() -> "Analysis":
+        return Analysis(
+            message_type_counts={},
+            incremental_snapshot_event_source_counts={},
+            mutation_addition_counts={},
+            mutation_attributes_counts={},
+            addition_sizes=[],
+            mutation_removal_count=SizedCount(0, 0),
+            text_mutation_count=SizedCount(0, 0),
+            unterminated_lines=[],
+        )
 
     def __add__(self, other: "Analysis") -> "Analysis":
         return Analysis(
-            mutation_removal_count=self.mutation_removal_count
-            + other.mutation_removal_count,
+            mutation_removal_count=self.mutation_removal_count.combine(
+                other.mutation_removal_count
+            ),
             addition_sizes=self.addition_sizes + other.addition_sizes,
             mutation_attributes_counts=_combine_sized_count_dicts(
                 self.mutation_attributes_counts,
@@ -134,29 +143,60 @@ class Analysis:
                 + other.message_type_counts.get(k, 0)
                 for k in set(self.message_type_counts) | set(other.message_type_counts)
             },
+            text_mutation_count=self.text_mutation_count.combine(
+                other.text_mutation_count
+            ),
             unterminated_lines=self.unterminated_lines + other.unterminated_lines,
         )
 
     def __str__(self) -> str:
+        mutation_overview = {
+            **self.mutation_addition_counts,
+            "removal": self.mutation_removal_count,
+            "text": self.text_mutation_count,
+            **self.mutation_attributes_counts,
+        }
+        total_count = 0
+        total_size = 0
+        for sized_count in mutation_overview.values():
+            total_count += sized_count.count
+            total_size += sized_count.size
+
         return f"""message_type_counts
-        {self.message_type_counts}
-        incremental_snapshot_event_source_counts
-        {self.incremental_snapshot_event_source_counts}
-        mutation_removal_count: {str(self.mutation_removal_count)}
-        mutation_addition_counts
-        {self.mutation_addition_counts}
-        mutation_attributes_counts"
-        {self.mutation_attributes_counts}
-        unterminated_lines
-        {self.unterminated_lines}
-        """
+{self.message_type_counts}
+incremental_snapshot_event_source_counts
+{self.incremental_snapshot_event_source_counts}
+    mutation_removal_count: {str(self.mutation_removal_count)}
+    mutation_addition_counts
+    {self.mutation_addition_counts}
+    mutation_attributes_counts"
+    {self.mutation_attributes_counts}
+    "text mutations"
+    {self.text_mutation_count}
+unterminated_lines_count
+{len(self.unterminated_lines)}
+unterminated_lines
+{self.unterminated_lines}
+
+Mutations:
+
+incremental_tracking: {self.incremental_snapshot_event_source_counts["Mutation"]}
+mutation overview:
+    total mutations: {total_count}
+    overall size: {sizeof_fmt(total_size)}
+{mutation_overview}
+"""
 
     def __repr__(self) -> str:
         return str(self)
 
 
 def analyse_exported_file(file_path: str) -> Analysis:
-    analysis = Analysis()
+    """
+    When operating on an "exported recording" then you have one file which has a snapshots key.
+    That is an array of JSON objects. each has a window id but is otherwise an rrweb event
+    """
+    analysis = Analysis.empty()
 
     with open(file_path, "r") as file:
         for list_of_snapshots in ijson.items(file, "data.snapshots"):
@@ -173,7 +213,7 @@ def analyse_s3_file(file_path: str) -> Analysis:
     Each has a window id and an array at the "data" key.
     Each item in that array is an rrweb event
     """
-    analysis = Analysis()
+    analysis = Analysis.empty()
 
     with open(file_path, "r") as file:
         line_index = -1
@@ -185,7 +225,8 @@ def analyse_s3_file(file_path: str) -> Analysis:
 
             try:
                 json_line = json.loads(line)
-                analysis += analyse_snapshots(json_line.get("data", []))
+                line_analysis = analyse_snapshots(json_line.get("data", []))
+                analysis += line_analysis
             except JSONDecodeError:
                 analysis.unterminated_lines.append(
                     UnterminatedLine(file_path, line_index, line[-20:])
@@ -194,12 +235,27 @@ def analyse_s3_file(file_path: str) -> Analysis:
     return analysis
 
 
+def ensure_all_mutation_types_are_handled(data: Dict) -> None:
+    handled_mutations = ["removes", "adds", "attributes", "texts"]
+    ignored_keys = ["source"]
+    ignore_list = handled_mutations + ignored_keys
+    mutations_present = data.keys()
+    unhandled_mutations = [
+        mutation for mutation in mutations_present if mutation not in ignore_list
+    ]
+
+    if unhandled_mutations:
+        print(f"Unhandled mutations in {unhandled_mutations} in '{data}'")
+        raise ValueError(f"Unhandled mutations: {unhandled_mutations}")
+
+
 # TODO ijson returns any :'(
 def analyse_snapshots(list_of_snapshots: any) -> Analysis:
-    analysis = Analysis()
+    analysis = Analysis.empty()
 
     for snapshot in list_of_snapshots:
         event_type = event_types[snapshot.get("type", -1)]
+
         if event_type not in analysis.message_type_counts:
             analysis.message_type_counts[event_type] = 0
         analysis.message_type_counts[event_type] += 1
@@ -211,44 +267,51 @@ def analyse_snapshots(list_of_snapshots: any) -> Analysis:
                     0, 0
                 )
             analysis.incremental_snapshot_event_source_counts[source_] += len(
-                json.dumps(snapshot["data"])
+                json.dumps(snapshot["data"], separators=(",", ":"))
             )
 
             if source_ == "Mutation":
-                analysis.mutation_removal_count += len(snapshot["data"]["removes"])
+                # mutations we handle
+                ensure_all_mutation_types_are_handled(snapshot["data"])
+
+                for removal in snapshot["data"]["removes"]:
+                    analysis.mutation_removal_count += len(
+                        json.dumps(removal, separators=(",", ":"))
+                    )
+
                 for addition in snapshot["data"]["adds"]:
                     node_type = node_types[addition["node"]["type"]]
                     if node_type not in analysis.mutation_addition_counts:
                         analysis.mutation_addition_counts[node_type] = SizedCount(0, 0)
-                    addition_size = len(json.dumps(addition))
+                    addition_size = len(json.dumps(addition, separators=(",", ":")))
                     analysis.mutation_addition_counts[node_type] += addition_size
                     analysis.addition_sizes.append(addition_size)
 
-                for altered_attribute in snapshot["data"]["attributes"]:
-                    # this is an array of dicts. each should have `attributes`
-                    # and that is a dict whose key is the attibute
-                    changeds = altered_attribute["attributes"].keys()
-                    for changed in changeds:
-                        if changed not in analysis.mutation_attributes_counts:
-                            analysis.mutation_attributes_counts[changed] = SizedCount(
-                                0, 0
-                            )
-                        analysis.mutation_attributes_counts[changed] += len(
-                            json.dumps(altered_attribute["attributes"][changed])
+                for mutated_attribute in snapshot["data"]["attributes"]:
+                    # attribute mutations come together in a dict
+                    # tracking them individually gives confusing counts
+                    attribute_fingerprint = "---".join(
+                        mutated_attribute["attributes"].keys()
+                    )
+
+                    if attribute_fingerprint not in analysis.mutation_attributes_counts:
+                        analysis.mutation_attributes_counts[
+                            attribute_fingerprint
+                        ] = SizedCount(0, 0)
+                    analysis.mutation_attributes_counts[attribute_fingerprint] += len(
+                        json.dumps(
+                            snapshot["data"]["attributes"], separators=(",", ":")
                         )
+                    )
+
+                for text in snapshot["data"]["texts"]:
+                    analysis.text_mutation_count += len(text)
 
     return analysis
 
 
 def analyse_recording(file_path: str, source: Literal["s3", "export"]) -> None:
-    """
-    When operating on an "exported recording" then you have one file which has a snapshots key.
-    That is an array of JSON objects. each has a window id but is otherwise an rrweb event
-
-
-    """
-
-    analysis: Analysis = Analysis()
+    analysis = Analysis.empty()
 
     if source == "export":
         analysis = analyse_exported_file(file_path)
@@ -256,7 +319,8 @@ def analyse_recording(file_path: str, source: Literal["s3", "export"]) -> None:
         # open each file in the provided directory
         sorted_files = sorted(os.listdir(file_path))
         for file_name in sorted_files:
-            analysis = analyse_s3_file(os.path.join(file_path, file_name))
+            print(f"processing file: {file_name}")
+            analysis += analyse_s3_file(os.path.join(file_path, file_name))
     else:
         raise ValueError(f"Unknown source {source}")
 
@@ -265,8 +329,8 @@ def analyse_recording(file_path: str, source: Literal["s3", "export"]) -> None:
 
 if __name__ == "__main__":
     # TODO get the file path from the command line
-    analyse_recording(
-        "/Users/paul/Downloads/export-018af09a-5995-7334-8970-ac2bf0821e92.ph-recording.json",
-        "export",
-    )
-    # analyse_recording("/Users/paul/Downloads/huge-recording/", "s3")
+    # analyse_recording(
+    #     "/Users/paul/Downloads/export-018af09a-5995-7334-8970-ac2bf0821e92.ph-recording.json",
+    #     "export",
+    # )
+    analyse_recording("/Users/paul/Downloads/huge-recording/", "s3")
